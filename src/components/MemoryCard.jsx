e037244a33dc6d08bf5ec4773295078d7cfefdc9
CandyTale55 <candytale55@gmail.com>
2025-09-18 07:08:16 +0200

    fix class vs style in MemoryCard and rename emojiEl to cardEl

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -2,17 +2,17 @@ import { decodeEntity } from 'html-entities'
 import EmojiButton from './EmojiButton'
 
 export default function MemoryCard({ handleClick, data }) {
-    const emojiEl = data.map((emoji, index) =>
+    const cardEl = data.map((emoji, index) =>
         <li key={index} className="card-item">
             <EmojiButton
                 content={decodeEntity(emoji.htmlCode[0])}
-                class="btn btn--emoji"
+                style="btn btn--emoji"
                 handleClick={() => handleClick(emoji.name, index)}
             />
         </li>
     )
 
-    return <ul className="card-container">{emojiEl}</ul>
+    return <ul className="card-container">{cardEl}</ul>
 }
 
 
